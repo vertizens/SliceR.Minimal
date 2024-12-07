8e4674bd3d0c7484a75e5aedb4e7187cc2e4db21
wilsonshack <wilsonshack@gmail.com>
2024-12-07 16:36:44 -0600

    feat: update nuget version and README

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # SliceR.Minimal
-Adds to SliceR .NET Minimal API patterns for quickly adding endpoints
+
+Adds Minimal API patterns for quickly adding endpoints to SliceR
 
 ## Getting Started
 
@@ -65,10 +66,18 @@ depending on the overload called.
 These have the same entity operation methods except now the Entity, Key, and Domain doesn't have to be repeated 
 for each route.
 
+For custom keys that select one property from a type then use `EntityKeySelectorRouteGroupBuilder` by calling `MapEntityRouteGroup<TEntity, TKey, TKeyProperty>`
+Any ..AsKey method uses the AsParameters attribute on the key in the minimal delegate so make sure the route pattern given has 
+the property names from the key type in use.
+
+Any EntityRouteGroupBuilder puts the `EntityMetadata<TEntity>` on the endpoint so when figuring out handlers that use a domain for a 
+response, the default registered handler is one that know what entity to use for querying.
 
 ## ValidatedResult
 
-If you use `MapRootRouteGroup` then the `ValidatedResultEndpointFilter` is added by default.  This simply turns a `ValidatedResult` into an `IResult` for the endpoint.  This means the result gets translated to what the Http result required so if you do custom things make sure you either perform a ToHttpResult() on the `ValidatedResult` or add the endpoint filter to the endpoint manually.
+If you use `MapRootRouteGroup` then the `ValidatedResultEndpointFilter` is added by default.  This simply turns a `ValidatedResult` into an `IResult` for the endpoint.
+  This means the result gets translated to what the Http result required so if you do custom things make sure you either perform a ToHttpResult() on the `ValidatedResult` or add the endpoint filter to the endpoint manually.
+Same applies to MapEntityRouteGroup as well.
 
 ## FileResponse
 
@@ -76,14 +85,17 @@ Use `ValidatedResult<FileResponse>` as the return value from a `ValidatedHandler
 
 ## Mapping Domain to Entity
 
-If you use EF Core and want to have a DTO that is returned the basic scenarios are already handled by defining endpoints return the DTO type but then add the interface `IDomainEntity<TEntity>` to your DTO where `TEntity` is the entity type.
+If you use EF Core and want to have a DTO that is returned the basic scenarios are already handled by defining endpoints return the DTO type 
+but then add the interface `IDomainEntity<TEntity>` to your DTO where `TEntity` is the entity type.
 
 These default handlers can be registered by calling:  
 
     services.AddSliceREndpointDefaultValidatedHandlers();
 
-The trick here is that the endpoint builders need to be registered first so the code can evaluate what default handler interfaces are being referenced.  It then attempts to match the desired type to an entity, if not an entity then checks if the type implements the `IEntityMetadata<TEntity>`.  If it all matches up then a default handler can be registered. 
+The trick here is that the endpoint builders need to be registered first so the code can evaluate what default handler interfaces are being referenced.  It then 
+attempts to match the desired type to an entity, if not an entity then checks if the type implements the `IEntityMetadata<TEntity>`.  If it all matches up then a default handler can be registered. 
 If using `EntityRouteGroupBuilder` then the endpoint itself will already contain EntityMetadata so the DTO does not need the interface itself.
 
 #### Debugging Note:
-Code was added to log to the console on startup for any handlers that don't exist.  The Microsoft developer exception page doesn't help with what type is missing when it can't be found in the services collection.  So its helpful to log this specific missing handler type at the beginning of the console log.
+Code was added to log to the console on startup for any handlers that don't exist.  The Microsoft developer exception page doesn't help with what type is missing 
+when it can't be found in the services collection.  So its helpful to log this specific missing handler type at the beginning of the console log.
---
+++ src/Vertizens.SliceR.Minimal/EndpointRouteBuilderExtensions.EntityKeySelectorRouteGroup.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+
+namespace Vertizens.SliceR.Minimal;
+public static partial class EndpointRouteBuilderExtensions
+{
+    /// <summary>
+    /// Creates <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty}"/> with prefix and tag to build endpoints for.
+    /// Used when there is a need to control the parameter name for the key instead of default 'id'.  Create a key type with a 
+    /// property that matches the key and expect AsParameters attribute to be used with it.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity with which to create a <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty}"/>.</typeparam>
+    /// <typeparam name="TKey">Key of the entity</typeparam>
+    /// <typeparam name="TKeyProperty"></typeparam>
+    /// <param name="builder">Instance of <see cref="IEndpointRouteBuilder"/></param>
+    /// <param name="keySelector">func to select key property from a key type</param>
+    public static EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty> MapEntityRouteGroup<TEntity, TKey, TKeyProperty>(this IEndpointRouteBuilder builder, Func<TKey, TKeyProperty> keySelector)
+        where TKey : notnull
+    {
+        return builder.MapEntityRouteGroup<TEntity, TKey, TKeyProperty>(typeof(TEntity).Name, keySelector);
+    }
+
+    /// <summary>
+    /// Creates <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty}"/> with prefix and tag to build endpoints for.
+    /// Used when there is a need to control the parameter name for the key instead of default 'id'.  Create a key type with a 
+    /// property that matches the key and expect AsParameters attribute to be used with it.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity with which to create a <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty}"/>.</typeparam>
+    /// <typeparam name="TKey">Key of the entity</typeparam>
+    /// <typeparam name="TKeyProperty"></typeparam>
+    /// <param name="builder">Instance of <see cref="IEndpointRouteBuilder"/></param>
+    /// <param name="routeGroupName">Custom route group name</param>
+    /// <param name="keySelector">func to select key property from a key type</param>
+    public static EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty> MapEntityRouteGroup<TEntity, TKey, TKeyProperty>(this IEndpointRouteBuilder builder, string routeGroupName, Func<TKey, TKeyProperty> keySelector)
+        where TKey : notnull
+    {
+        var groupTag = routeGroupName;
+        routeGroupName = JsonNamingPolicy.KebabCaseLower.ConvertName(routeGroupName);
+        return builder.MapEntityRouteGroup<TEntity, TKey, TKeyProperty>(routeGroupName, keySelector, ApiRouteGroupPrefix.Api, groupTag);
+    }
+
+    /// <summary>
+    /// Creates <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty}"/> with prefix and tag to build endpoints for.
+    /// Used when there is a need to control the parameter name for the key instead of default 'id'.  Create a key type with a 
+    /// property that matches the key and expect AsParameters attribute to be used with it.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity with which to create a <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty}"/>.</typeparam>
+    /// <typeparam name="TKey">Key of the entity</typeparam>
+    /// <typeparam name="TKeyProperty"></typeparam>
+    /// <param name="builder">Instance of <see cref="IEndpointRouteBuilder"/></param>
+    /// <param name="keySelector">func to select key property from a key type</param>
+    /// <param name="groupPrefix">set a group prefix</param>
+    /// <param name="groupTag">define the group tag</param>
+    public static EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty> MapEntityRouteGroup<TEntity, TKey, TKeyProperty>(
+        this IEndpointRouteBuilder builder,
+        Func<TKey, TKeyProperty> keySelector,
+        [StringSyntax("Route")] string? groupPrefix = null,
+        string? groupTag = null
+        ) where TKey : notnull
+    {
+        var entityType = typeof(TEntity);
+        var routeGroupName = JsonNamingPolicy.KebabCaseLower.ConvertName(entityType.Name);
+
+        return builder.MapEntityRouteGroup<TEntity, TKey, TKeyProperty>(routeGroupName, keySelector, groupPrefix, groupTag);
+    }
+
+    /// <summary>
+    /// Creates <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty}"/> with prefix and tag to build endpoints for.
+    /// Used when there is a need to control the parameter name for the key instead of default 'id'.  Create a key type with a 
+    /// property that matches the key and expect AsParameters attribute to be used with it.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity with which to create a <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty}"/>.</typeparam>
+    /// <typeparam name="TKey">Key of the entity</typeparam>
+    /// <typeparam name="TKeyProperty"></typeparam>
+    /// <param name="builder">Instance of <see cref="IEndpointRouteBuilder"/></param>
+    /// <param name="routeGroupName">Custom route group name</param>
+    /// <param name="keySelector">func to select key property from a key type</param>
+    /// <param name="groupPrefix">set a group prefix</param>
+    /// <param name="groupTag">define the group tag</param>
+    public static EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty> MapEntityRouteGroup<TEntity, TKey, TKeyProperty>(
+        this IEndpointRouteBuilder builder,
+        [StringSyntax("Route")] string routeGroupName,
+        Func<TKey, TKeyProperty> keySelector,
+        [StringSyntax("Route")] string? groupPrefix = null,
+        string? groupTag = null
+        ) where TKey : notnull
+    {
+        var prefix = groupPrefix != null ? string.Join('/', groupPrefix, routeGroupName) : routeGroupName;
+        var routeGroup = builder.MapGroup(prefix);
+
+        if (!string.IsNullOrWhiteSpace(groupTag))
+        {
+            routeGroup.WithTags(groupTag);
+        }
+
+        routeGroup.AddEndpointFilter<ValidatedResultEndpointFilter>();
+
+        return new EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty>(routeGroup, keySelector);
+    }
+
+    /// <summary>
+    /// Creates <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty,TDomain}"/> with prefix and tag to build endpoints for.
+    /// Used when there is a need to control the parameter name for the key instead of default 'id'.  Create a key type with a 
+    /// property that matches the key and expect AsParameters attribute to be used with it.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity with which to create a <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty,TDomain}"/>.</typeparam>
+    /// <typeparam name="TKey">Key of the entity</typeparam>
+    /// <typeparam name="TKeyProperty"></typeparam>
+    /// <typeparam name="TDomain">Domain type that will be returned in place of entity</typeparam>
+    /// <param name="builder">Instance of <see cref="IEndpointRouteBuilder"/></param>
+    /// <param name="keySelector">func to select key property from a key type</param>
+    public static EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty, TDomain> MapEntityRouteGroup<TEntity, TKey, TKeyProperty, TDomain>(this IEndpointRouteBuilder builder, Func<TKey, TKeyProperty> keySelector)
+    where TKey : notnull
+    {
+        return builder.MapEntityRouteGroup<TEntity, TKey, TKeyProperty, TDomain>(typeof(TEntity).Name, keySelector);
+    }
+
+    /// <summary>
+    /// Creates <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty,TDomain}"/> with prefix and tag to build endpoints for.
+    /// Used when there is a need to control the parameter name for the key instead of default 'id'.  Create a key type with a 
+    /// property that matches the key and expect AsParameters attribute to be used with it.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity with which to create a <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty,TDomain}"/>.</typeparam>
+    /// <typeparam name="TKey">Key of the entity</typeparam>
+    /// <typeparam name="TKeyProperty"></typeparam>
+    /// <typeparam name="TDomain">Domain type that will be returned in place of entity</typeparam>
+    /// <param name="builder">Instance of <see cref="IEndpointRouteBuilder"/></param>
+    /// <param name="routeGroupName">Custom route group name</param>
+    /// <param name="keySelector">func to select key property from a key type</param>
+    public static EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty, TDomain> MapEntityRouteGroup<TEntity, TKey, TKeyProperty, TDomain>(this IEndpointRouteBuilder builder, string routeGroupName, Func<TKey, TKeyProperty> keySelector)
+        where TKey : notnull
+    {
+        var groupTag = routeGroupName;
+        routeGroupName = JsonNamingPolicy.KebabCaseLower.ConvertName(routeGroupName);
+        return builder.MapEntityRouteGroup<TEntity, TKey, TKeyProperty, TDomain>(routeGroupName, keySelector, ApiRouteGroupPrefix.Api, groupTag);
+    }
+
+    /// <summary>
+    /// Creates <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty,TDomain}"/> with prefix and tag to build endpoints for.
+    /// Used when there is a need to control the parameter name for the key instead of default 'id'.  Create a key type with a 
+    /// property that matches the key and expect AsParameters attribute to be used with it.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity with which to create a <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty,TDomain}"/>.</typeparam>
+    /// <typeparam name="TKey">Key of the entity</typeparam>
+    /// <typeparam name="TKeyProperty"></typeparam>
+    /// <typeparam name="TDomain">Domain type that will be returned in place of entity</typeparam>
+    /// <param name="builder">Instance of <see cref="IEndpointRouteBuilder"/></param>
+    /// <param name="keySelector">func to select key property from a key type</param>
+    /// <param name="groupPrefix">set a group prefix</param>
+    /// <param name="groupTag">define the group tag</param>
+    public static EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty, TDomain> MapEntityRouteGroup<TEntity, TKey, TKeyProperty, TDomain>(
+        this IEndpointRouteBuilder builder,
+        Func<TKey, TKeyProperty> keySelector,
+        [StringSyntax("Route")] string? groupPrefix = null,
+        string? groupTag = null
+        ) where TKey : notnull
+    {
+        var entityType = typeof(TEntity);
+        var routeGroupName = JsonNamingPolicy.KebabCaseLower.ConvertName(entityType.Name);
+
+        return builder.MapEntityRouteGroup<TEntity, TKey, TKeyProperty, TDomain>(routeGroupName, keySelector, groupPrefix, groupTag);
+    }
+
+    /// <summary>
+    /// Creates <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty,TDomain}"/> with prefix and tag to build endpoints for.
+    /// Used when there is a need to control the parameter name for the key instead of default 'id'.  Create a key type with a 
+    /// property that matches the key and expect AsParameters attribute to be used with it.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity with which to create a <see cref="EntityKeySelectorRouteGroupBuilder{TEntity,TKey,TKeyProperty,TDomain}"/>.</typeparam>
+    /// <typeparam name="TKey">Key of the entity</typeparam>
+    /// <typeparam name="TKeyProperty"></typeparam>
+    /// <typeparam name="TDomain">Domain type that will be returned in place of entity</typeparam>
+    /// <param name="builder">Instance of <see cref="IEndpointRouteBuilder"/></param>
+    /// <param name="routeGroupName">Custom route group name</param>
+    /// <param name="keySelector">func to select key property from a key type</param>
+    /// <param name="groupPrefix">set a group prefix</param>
+    /// <param name="groupTag">define the group tag</param>
+    public static EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty, TDomain> MapEntityRouteGroup<TEntity, TKey, TKeyProperty, TDomain>(
+        this IEndpointRouteBuilder builder,
+        [StringSyntax("Route")] string routeGroupName,
+        Func<TKey, TKeyProperty> keySelector,
+        [StringSyntax("Route")] string? groupPrefix = null,
+        string? groupTag = null
+        ) where TKey : notnull
+    {
+        var prefix = groupPrefix != null ? string.Join('/', groupPrefix, routeGroupName) : routeGroupName;
+        var routeGroup = builder.MapGroup(prefix);
+
+        if (!string.IsNullOrWhiteSpace(groupTag))
+        {
+            routeGroup.WithTags(groupTag);
+        }
+
+        routeGroup.AddEndpointFilter<ValidatedResultEndpointFilter>();
+
+        return new EntityKeySelectorRouteGroupBuilder<TEntity, TKey, TKeyProperty, TDomain>(routeGroup, keySelector);
+    }
+}
